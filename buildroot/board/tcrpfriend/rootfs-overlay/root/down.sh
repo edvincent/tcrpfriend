e18edee64d4a5e20aa467db40c2a75fe861ca303
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-11-26 18:21:20 +0900

    Update down.sh

--- buildroot/board/tcrpfriend/rootfs-overlay/root/down.sh
+++ buildroot/board/tcrpfriend/rootfs-overlay/root/down.sh
@@ -1,7 +1,7 @@
 curl -kL "https://github.com/PeterSuh-Q3/tcrpfriend/raw/main/buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh" -O
 
 HOMEPATH="/root/"
-TOOLSPATH="https://raw.githubusercontent.com/PeterSuh-Q3/tcrpfriend/buildroot/board/tcrpfriend/rootfs-overlay/root/tools/"
+TOOLSPATH="https://raw.githubusercontent.com/PeterSuh-Q3/tcrpfriend/main/buildroot/board/tcrpfriend/rootfs-overlay/root/tools/"
 TOOLS="bspatch bzImage-template-v4.gz bzImage-template-v5.gz bzImage-to-vmlinux.sh calc_run_size.sh crc32 dtc kexec kpatch ramdisk-patch.sh vmlinux vmlinux-to-bzImage.sh xxd"
 
 echo "Downloading Kernel Patch tools"
