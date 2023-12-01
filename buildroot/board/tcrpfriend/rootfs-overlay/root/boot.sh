828f2f5068525597138e59ede4305b32eef80754
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-12-01 15:43:59 +0900

    Update boot.sh

--- buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh
+++ buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh
@@ -702,8 +702,9 @@ function setmac() {
         curmac=$(ip link | grep -A 1 ${eth} | tail -1 | awk '{print $2}' | sed -e 's/://g' | tr '[:lower:]' '[:upper:]')
         usrmac=mac${I}
         MAC="${usrmac:0:2}:${usrmac:2:2}:${usrmac:4:2}:${usrmac:6:2}:${usrmac:8:2}:${usrmac:10:2}"
+        DRIVER=$(ls -ld /sys/class/net/${eth}/device/driver 2>/dev/null | awk -F '/' '{print $NF}')
         if [ -n "${usrmac}" ]; then
-            echo "Setting MAC from ${curmac} to ${MAC}" | tee -a boot.log
+            echo "Setting MAC Address from ${curmac} to ${MAC} on ${eth} (${DRIVER}) " | tee -a boot.log
             ip link set dev ${eth} address ${MAC} >/dev/null 2>&1 
         fi
         I=$((${I} + 1))
