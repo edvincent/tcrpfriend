a461a415250b6274374198dc00cf7df80bef0602
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-11 13:43:29 +0900

    Update load-sda-first.sh

--- buildroot/board/tcrpfriend/rootfs-overlay/root/load-sda-first.sh
+++ buildroot/board/tcrpfriend/rootfs-overlay/root/load-sda-first.sh
@@ -3,7 +3,6 @@
 echo "Mounting /root/boot-image-dummy-sda.img to /dev/sda"
 imgpath="/root/boot-image-dummy-sda.img"
 gunzip -f "${imgpath}.gz"
-dd if=${imgpath} of=/dev/sda
-#LOOPX=$(losetup -f)
-#losetup -P ${LOOPX} ${imgpath}
-#ln -s ${LOOPX}  /dev/sda
+LOOPX=$(losetup -f)
+losetup -P ${LOOPX} ${imgpath}
+ln -s ${LOOPX}  /dev/sda
