0b91c4d542011007a0fe0f64e90b3cad9508cca6
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-12 18:12:49 +0900

    Update load-sda-first.sh

--- buildroot/board/tcrpfriend/rootfs-overlay/root/load-sda-first.sh
+++ buildroot/board/tcrpfriend/rootfs-overlay/root/load-sda-first.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Mounting /root/boot-image-dummy-sda.img to /dev/sda"
+echo "Mounting /root/boot-image-dummy-sda.img to /dev/loop0" >> /var/log/messages
 imgpath="/root/boot-image-dummy-sda.img"
-gunzip -f "${imgpath}.gz"
-insmod /lib/modules/loop.ko
-losetup -P /dev/loop0 ${imgpath}
+gunzip -f "${imgpath}.gz" >> /var/log/messages
+insmod /lib/modules/loop.ko >> /var/log/messages
+echo "Before losetup /root/boot-image-dummy-sda.img" >> /var/log/messages
+blkid | grep msdos >> /var/log/messages
+losetup -P /dev/loop0 ${imgpath} 
+echo "After losetup /root/boot-image-dummy-sda.img" >> /var/log/messages
+blkid | grep msdos >> /var/log/messages
