e6cda3e718b16290dfe7d632d9aac15aba3e9391
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-22 12:33:59 +0900

    Delete buildroot/board/tcrpfriend/rootfs-overlay/root/load-sda-first.sh

--- buildroot/board/tcrpfriend/rootfs-overlay/root/load-sda-first.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Mounting /root/boot-image-dummy-sda.img to /dev/loop0"
-imgpath="/root/boot-image-dummy-sda.img"
-gunzip -f "${imgpath}.gz" >> /var/log/messages
-losetup -P /dev/loop0 ${imgpath} 
