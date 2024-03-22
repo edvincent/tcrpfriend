fcf1f775c6860dadc1a421751ab6f9e79a8d23ee
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-23 00:53:13 +0900

    Update boot.sh

--- buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh
+++ buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh
@@ -886,8 +886,8 @@ function mountall() {
         fi    
     done
     if [ -z "${LOADER_DISK}" ]; then
-        for edisk in $(sudo fdisk -l | grep "Disk /dev/nvme" | awk '{print $2}' | sed 's/://' ); do
-            if [ $(sudo fdisk -l | grep "83 Linux" | grep ${edisk} | wc -l ) -eq 3 ]; then
+        for edisk in $(fdisk -l | grep "Disk /dev/nvme" | awk '{print $2}' | sed 's/://' ); do
+            if [ $(fdisk -l | grep "83 Linux" | grep ${edisk} | wc -l ) -eq 3 ]; then
                 LOADER_DISK="$(blkid | grep ${edisk} | grep "6234-C863" | cut -c 1-12 | awk -F\/ '{print $3}')"    
             fi    
         done
