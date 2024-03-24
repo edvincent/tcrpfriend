56f5ca4ecaec6acbae34b4dc102f1116dac19d50
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-03-24 17:23:20 +0900

    Update boot.sh

--- buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh
+++ buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh
@@ -833,8 +833,8 @@ function getip() {
             fi
             COUNT=$((${COUNT} + 1))
             if [ $(ip route | grep default | grep metric | grep ${eth} | wc -l) -eq 1 ]; then
-                IP="$(ip route show dev ${eth} 2>/dev/null | grep default | awk '{print $7}')"
-                #IP="$(ip route get 1.1.1.1 2>/dev/null | grep ${eth} | awk '{print $7}')"
+                #IP="$(ip route show dev ${eth} 2>/dev/null | grep default | awk '{print $7}')"
+                IP="$(ip route get 1.1.1.1 2>/dev/null | grep ${eth} | awk '{print $7}')"
                 IP=$(echo -n "${IP}" | tr '\n' '\b')
                 LASTIP="${IP}"
                 break
@@ -845,6 +845,9 @@ function getip() {
         done
         if [ -n "${1}" ]; then
             if [ -n "${IP}" ] && [ "${eth}" = "${ethdev}" ]; then
+                echo "IP = ${IP}"
+                echo "eth = ${eth}"
+                echo "staticip = ${1}"
                 ip a del "${IP}" dev ${eth}
                 ip a add "${1}" dev ${eth}
                 ip a
