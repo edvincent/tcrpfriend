32ac8bf367c42e10413c778cc91e20e690847669
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-04-01 23:39:14 +0900

    Update boot.sh

--- buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh
+++ buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh
@@ -99,8 +99,7 @@ function showlastupdate() {
 0.1.0t Supports bootloader injection with SHR disk only
        dom_szmax=32GB (limit size of the injected bootloader)
 0.1.0u Loader support bus type expansion (mmc, NVMe, etc.)
-0.1.0v Improved functionality to skip non-bootloader devices
-0.1.0w Improved setnetwork function for using static IP
+0.1.0x Multilingual explanation support
 
 EOF
 }
