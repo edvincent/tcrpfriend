f969a7f655d3c8059a3c6704e3674acf2733ff57
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-04-10 22:24:28 +0900

    Create buildroot.sh

---
+++ buildroot.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/buildroot/buildroot.git /opt/buildroot
+cd /opt/buildroot
+sudo apt-get update
+sudo apt-get install -y libelf-dev locales busybox dialog curl xz-utils cpio sed
+mkdir /opt/config-files
+cd /opt/config-files
+git clone -b master https://github.com/PeterSuh-Q3/redpill-load.git
+mkdir /opt/firmware
+cd /opt/buildroot
+git checkout 2023.08.4
+git pull origin 2023.08.4
+cp -rf /home/runner/work/tcrpfriend/tcrpfriend/buildroot/* .
+cp -rf /opt/config-files/redpill-load/config /home/runner/work/tcrpfriend/tcrpfriend/buildroot/board/tcrpfriend/rootfs-overlay/root/
+chmod 777 board/tcrpfriend/rootfs-overlay/root/*.sh
+chmod 777 board/tcrpfriend/rootfs-overlay/root/tools/*
