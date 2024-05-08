92078a71c7e8cacc1c8750e984bfb8c9a9c119b8
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-05-08 23:38:19 +0900

    Update makepo.sh

--- buildroot/board/tcrpfriend/rootfs-overlay/root/makepo.sh
+++ buildroot/board/tcrpfriend/rootfs-overlay/root/makepo.sh
@@ -1,4 +1,4 @@
-for L in `echo "en_US ko_KR ja_JP zh_CN ru_RU fr_FR de_DE es_ES it_IT pt_BR"`; do
+for L in `echo "en_US ko_KR ja_JP zh_CN ru_RU fr_FR de_DE es_ES it_IT pt_BR ar_EG hi_IN hu_HU id_ID tr_TR am_ET th_TH"`; do
 	[ ! -d "lang/${L}/LC_MESSAGES" ] && mkdir -p lang/${L}/LC_MESSAGES
 	echo "dante9000@gmail.com" | msginit -i lang/lang.pot -l ${L}.UTF-8 -o lang/${L}/LC_MESSAGES/msg.po
 done
